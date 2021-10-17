55d723e03e9df3200ce50e70e49c8b0c7f6a1748
Maaruni <maarunip.2020@scis.smu.edu.sg>
2021-10-17 12:46:35 +0800

    second deploy

--- app.py
+++ app.py
@@ -1,26 +1,63 @@
-# https://docs.streamlit.io/library/get-started/create-an-app
+# # https://docs.streamlit.io/library/get-started/create-an-app
 
+# import streamlit as st
+# import pandas as pd
+# import numpy as np
+
+# st.title('Uber pickups in NYC')
+
+
+# DATE_COLUMN = 'date/time'
+# DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+#          'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+# def load_data(nrows):
+#     data = pd.read_csv(DATA_URL, nrows=nrows)
+#     lowercase = lambda x: str(x).lower()
+#     data.rename(lowercase, axis='columns', inplace=True)
+#     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+#     return data
+
+# # Create a text element and let the reader know the data is loading.
+# data_load_state = st.text('Loading data...')
+# # Load 10,000 rows of data into the dataframe.
+# data = load_data(10000)
+# # Notify the reader that the data was successfully loaded.
+# data_load_state.text('Loading data...done!')
+
+
+from vega_datasets import data
 import streamlit as st
-import pandas as pd
-import numpy as np
+import altair as alt
 
-st.title('Uber pickups in NYC')
+def main():
+    df = load_data()
+    page = st.sidebar.selectbox("Choose a page", ["Homepage", "Exploration"])
 
+    if page == "Homepage":
+        st.header("This is your data explorer.")
+        st.write("Please select a page on the left.")
+        st.write(df)
+    elif page == "Exploration":
+        st.title("Data Exploration")
+        x_axis = st.selectbox("Choose a variable for the x-axis", df.columns, index=3)
+        y_axis = st.selectbox("Choose a variable for the y-axis", df.columns, index=4)
+        visualize_data(df, x_axis, y_axis)
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+@st.cache
+def load_data():
+    df = data.cars()
+    return df
 
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+def visualize_data(df, x_axis, y_axis):
+    graph = alt.Chart(df).mark_circle(size=60).encode(
+        x=x_axis,
+        y=y_axis,
+        color='Origin',
+        tooltip=['Name', 'Origin', 'Horsepower', 'Miles_per_Gallon']
+    ).interactive()
 
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
-data = load_data(10000)
-# Notify the reader that the data was successfully loaded.
-data_load_state.text('Loading data...done!')
+    st.write(graph)
+
+if __name__ == "__main__":
+    main()
